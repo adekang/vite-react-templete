6903d321fef0c7f9692b23b7cf63ba99b7ca326f
adekang <adekang@163.com>
2022-03-25 22:22:42 +0800

    fix: husky fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div className="App">
-            <p>
-                <button type="button" onClick={() => setCount((count) => count + 1)}>
-                    count is: {count}
-                </button>
-            </p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+    </div>
+  );
 }
 
 export default App;
