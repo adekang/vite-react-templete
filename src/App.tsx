7fe454171f69d82d9f8f8d4008b085e525b3d7a1
adekang <adekang@163.com>
2022-03-25 20:23:15 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
